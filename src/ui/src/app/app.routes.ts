51627b7a9a7d746db6ff00f2a7306bb6f1c10c29
Alin-Florin Ciu <alinflorinciu@icloud.com>
2025-07-27 15:46:08 +0300

    -

--- src/ui/src/app/app.routes.ts
+++ src/ui/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { Home } from './routes/home/home';
+import { About } from './routes/about/about';
+import { NotFound } from './routes/not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home
+  },
+  {
+    path: 'about',
+    component: About
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    component: NotFound
+  }
+];
