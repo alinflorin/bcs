152e4a15fbb1c64d69600e2ae4d4b030125b7d68
Alin-Florin Ciu <alinflorinciu@icloud.com>
2025-07-27 10:01:35 +0300

    d

--- src/api/Controllers/WeatherForecastController.cs
+++ src/api/Controllers/WeatherForecastController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Bcs.Api.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api/[controller]")]
 public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
