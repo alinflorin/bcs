b99afae951aecd4c3c07b8c4a1c281a08da740eb
Alin-Florin Ciu <alinflorinciu@icloud.com>
2025-07-28 13:56:40 +0300

    -

--- src/ui/src/app/app.config.ts
+++ src/ui/src/app/app.config.ts
@@ -1,16 +1,27 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import {TranslateModule, TranslateLoader} from "@ngx-translate/core";
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 import { routes } from './app.routes';
 import { authConfig } from './auth.config';
 import { authInterceptor, provideAuth } from 'angular-auth-oidc-client';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { HttpClient, provideHttpClient, withInterceptors } from '@angular/common/http';
+
+const httpLoaderFactory: (http: HttpClient) => TranslateHttpLoader = (http: HttpClient) =>
+    new TranslateHttpLoader(http, './i18n/', '.json');
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideHttpClient(withInterceptors([authInterceptor()])),
-    provideRouter(routes), provideAuth(authConfig)
+    provideRouter(routes), provideAuth(authConfig),
+    importProvidersFrom([TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: httpLoaderFactory,
+        deps: [HttpClient],
+      },
+    })])
   ]
 };
--- src/ui/src/app/app.ts
+++ src/ui/src/app/app.ts
@@ -4,22 +4,46 @@ import { Header } from './layout/header/header';
 import { Footer } from './layout/footer/footer';
 import { OidcSecurityService } from 'angular-auth-oidc-client';
 import { ApiService } from './services/api.service';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer],
+  imports: [RouterOutlet, Header, Footer, TranslateModule],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App implements OnInit {
   private readonly oidcSecurityService = inject(OidcSecurityService);
   private readonly apiService = inject(ApiService);
+  private readonly translate = inject(TranslateService);
   apiVersion = signal('?');
 
   ngOnInit(): void {
+    // Auth
     this.oidcSecurityService.checkAuth().subscribe();
+
+    // Get API version
     this.apiService.getApiVersion().subscribe(r => {
       this.apiVersion.set(r.version);
     });
+
+    // Translate
+    this.translate.addLangs(['ro', 'en']);
+    this.translate.setDefaultLang('en');
+    this.translate.use(localStorage.getItem('lang') || this.translate.getBrowserLang() || 'en');
+  }
+
+  changeLanguage(newLang: string): void {
+    this.translate.use(newLang).subscribe(() => {
+      localStorage.setItem('lang', newLang);
+    });
+  }
+
+  login() {
+    this.oidcSecurityService.authorize();
+  }
+
+  logout() {
+    this.oidcSecurityService.logoff().subscribe();
   }
 }
