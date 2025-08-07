79366f05532e58b69800deeceb5a99159b240dda
Alin-Florin Ciu <alinflorinciu@icloud.com>
2025-08-07 12:55:44 +0300

    -

--- src/ui/src/app/services/admin.guard.ts
+++ src/ui/src/app/services/admin.guard.ts
@@ -1,17 +1,13 @@
 import { inject } from '@angular/core';
 import { CanActivateFn } from '@angular/router';
-import { OidcSecurityService } from 'angular-auth-oidc-client';
 import { map } from 'rxjs';
+import { AuthService } from './auth.service';
 
 export const adminGuard: CanActivateFn = (route, state) => {
-  const oidcService = inject(OidcSecurityService);
-  return oidcService.getPayloadFromAccessToken().pipe(
-    map(p => {
-      if (!p || !p.permissions) {
-        return false;
-      }
-      const permissions: string[] = p.permissions;
-      return permissions.includes("api:admin");
+  const authService = inject(AuthService);
+  return authService.user.pipe(
+    map(u => {
+      return !!u && u.isAdmin;
     })
   );
 };
