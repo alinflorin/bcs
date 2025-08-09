2215d9743af38288536c78cf5b889b3d1348c921
Alin-Florin Ciu <alinflorinciu@icloud.com>
2025-08-09 07:29:20 +0300

    -

--- src/ui/src/app/app.routes.ts
+++ src/ui/src/app/app.routes.ts
@@ -5,6 +5,8 @@ import { NotFound } from './routes/not-found/not-found';
 import { OauthCallback } from './routes/oauth-callback/oauth-callback';
 import { Settings } from './routes/settings/settings';
 import { autoLoginPartialRoutesGuard } from 'angular-auth-oidc-client';
+import { Admin } from './routes/admin/admin';
+import { adminGuard } from './services/admin.guard';
 
 export const routes: Routes = [
   {
@@ -23,6 +25,11 @@ export const routes: Routes = [
     path: 'settings',
     component: Settings,
     canActivate: [autoLoginPartialRoutesGuard]
+  },
+  {
+    path: 'admin',
+    component: Admin,
+    canActivate: [autoLoginPartialRoutesGuard, adminGuard]
   },
   {
     path: '**',
