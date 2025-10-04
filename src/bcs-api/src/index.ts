d23b74e537504ea5194b8934d3f120d77f8c71d6
Alin Florin <alinflorinciu@icloud.com>
2025-10-04 03:31:17 +0300

    d

--- src/bcs-api/src/index.ts
+++ src/bcs-api/src/index.ts
@@ -3,6 +3,10 @@ import express from 'express';
 const app = express();
 app.use(express.json());
 
+app.get('/api/health', (req, res) => {
+  res.json({ status: 'OK' });
+});
+
 app.get('/api/hello', (req, res) => {
   res.json({ message: 'Hello, world!' });
 });
