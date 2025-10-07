87b8d5ffa17a388f98ac3f75ea7fcebcfc1930be
Alin Florin <alinflorinciu@icloud.com>
2025-10-08 00:26:05 +0300

    -

--- src/bcs-ui/src/App.tsx
+++ src/bcs-ui/src/App.tsx
@@ -1,36 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { version } from './version'
+import { Layout, Grid } from "antd";
+import { Outlet } from "react-router";
+import Sidebar from "./components/Sidebar";
 
-function App() {
-  const [count, setCount] = useState(0)
+const { Content } = Layout;
+
+const App: React.FC = () => {
+  const screen = Grid.useBreakpoint();
+  const padding = screen.md ? 10 : 50;
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}. version is { version }
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Layout>
+      <Sidebar />
+      <Layout>
+        <Content style={{ paddingLeft: '10px', paddingTop: `${padding}px` }}>
+          <Outlet />
+        </Content>
+      </Layout>
+    </Layout>
+  );
+};
 
-export default App
+export default App;
--- src/bcs-ui/src/main.tsx
+++ src/bcs-ui/src/main.tsx
@@ -1,10 +1,32 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { BrowserRouter, Route, Routes } from "react-router";
+import Home from "./routes/Home.tsx";
+import Settings from "./routes/Settings.tsx";
+import "@ant-design/v5-patch-for-react-19";
+import { ConfigProvider } from "antd";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+  <ConfigProvider
+    theme={{
+      token: {
+        // Seed Token
+        colorPrimary: "#00b96b",
+        borderRadius: 2,
+
+        // Alias Token
+        colorBgContainer: "#f6ffed",
+      },
+    }}
+  >
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="settings" element={<Settings />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </ConfigProvider>
+);
