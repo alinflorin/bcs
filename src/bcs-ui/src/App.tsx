b55e9055e8af47f6109d94441a26c3a84de7e94a
Alin-Florin Ciu <alinflorinciu@icloud.com>
2025-10-09 17:19:55 +0300

    qwd

--- src/bcs-ui/src/App.tsx
+++ src/bcs-ui/src/App.tsx
@@ -1,22 +1,14 @@
-import { Layout, Grid } from "antd";
 import { Outlet } from "react-router";
 import Sidebar from "./components/Sidebar";
 
-const { Content } = Layout;
 
-const App: React.FC = () => {
-  const screen = Grid.useBreakpoint();
-  const padding = screen.md ? 10 : 50;
+const App = () => {
 
   return (
-    <Layout>
+    <div>
       <Sidebar />
-      <Layout>
-        <Content style={{ paddingLeft: '10px', paddingTop: `${padding}px` }}>
-          <Outlet />
-        </Content>
-      </Layout>
-    </Layout>
+      <Outlet />
+    </div>
   );
 };
 
