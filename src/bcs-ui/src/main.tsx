a8a5e743f4bedf55bfe87ee5c91bfb5a062b8913
Veronica Dinu <veronica@qoffice.ro>
2025-10-16 23:22:38 +0300

    part1

--- src/bcs-ui/src/main.tsx
+++ src/bcs-ui/src/main.tsx
@@ -5,6 +5,9 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import Home from "./routes/Home.tsx";
 import Settings from "./routes/Settings.tsx";
 import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import { AuthProvider } from "react-oidc-context";
+import Private from "./components/Private.tsx";
+import Chat from "./components/Chat.tsx";
 
 const theme = createTheme({
   palette: {
@@ -14,16 +17,33 @@ const theme = createTheme({
   },
 });
 
+const oidcConfig = {
+  authority: "https://dev-kpiuw0wghy7ta8x8.us.auth0.com",
+  client_id: "ZB6G0jz2KVW6acAmLhdibQ5ykL02YcBy",
+  redirect_uri: window.location.origin,
+  response_type: "code",
+  scope: "openid profile email",
+  onSigninCallback: () => {
+    // Remove query params from URL
+    window.history.replaceState({}, document.title, "/");
+  },
+  // ...
+};
+
 createRoot(document.getElementById("root")!).render(
   <ThemeProvider theme={theme}>
     <CssBaseline />
+  <AuthProvider {...oidcConfig}>
     <BrowserRouter>
       <Routes>
         <Route element={<App />}>
           <Route index element={<Home />} />
-          <Route path="settings" element={<Settings />} />
+          <Route path="settings" element={<Private><Settings /></Private>} />
+          <Route path="chat/:id" element={<Private><Chat /></Private>} />
         </Route>
       </Routes>
     </BrowserRouter>
+    </AuthProvider>
+
   </ThemeProvider>
 );
