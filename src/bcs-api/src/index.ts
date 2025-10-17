213748580010501c811616e2c8d8f61befb9e82e
Veronica Dinu <veronica@qoffice.ro>
2025-10-17 17:11:09 +0300

    -

--- src/bcs-api/src/index.ts
+++ src/bcs-api/src/index.ts
@@ -2,9 +2,29 @@ import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
 import { version } from './version';
+import { MongoClient } from 'mongodb';
+import { expressjwt as jwt } from 'express-jwt';
+import jwksRsa from 'jwks-rsa';
+
+const url = `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOSTNAME}:27017/bcs`;
+const client = new MongoClient(url);
+const beaza = client.db('bcs')
+
 
 const app = express();
 app.use(express.json());
+
+app.use(jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https:///.well-known/jwks.json`,
+  }),
+  audience: 'https://bcs-api',
+  issuer: `https://auth0/`,
+  algorithms: ['RS256'],
+}));
 
 app.get('/api/health', (req, res) => {
   res.json({ status: 'OK', version: version });
@@ -13,6 +33,10 @@ app.get('/api/health', (req, res) => {
 app.get('/api/hello', (req, res) => {
   res.json({ message: 'Hello, world! Version: ' + version });
 });
+
+
+app.post('/api/chat/new',  async (req, res)=>{
+})
 
 app.listen(8080, () => {
   console.log('Server is running on http://localhost:8080');
