21ff63404cfda41bbf68eb05c20aa897e90b38a3
Alin-Florin Ciu <alinflorinciu@icloud.com>
2025-10-18 10:54:36 +0300

    d

--- src/bcs-api/src/index.ts
+++ src/bcs-api/src/index.ts
@@ -8,7 +8,7 @@ import jwksRsa from "jwks-rsa";
 
 const url = `mongodb://${
   process.env.MONGODB_USERNAME
-    ? process.env.MONGODB_USERNAME + `:` + process.env.MONGODB_PASSWORD
+    ? process.env.MONGODB_USERNAME + `:` + process.env.MONGODB_PASSWORD + "@"
     : ``
 }${process.env.MONGODB_HOSTNAME || "localhost"}:${process.env.MONGODB_PORT || 27017}/${process.env.MONGODB_DATABASE || "bcs"}`;
 const client = new MongoClient(url);
