7466ae7fd2a91d2f342880e9e646ae5d3f5764db
Alin-Florin Ciu <alinflorinciu@icloud.com>
2025-10-18 11:24:44 +0300

    d

--- src/bcs-ui/entrypoint.sh
+++ src/bcs-ui/entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Generate runtime env file
-envsubst < /caddy-runtime/env-config.js.template > /caddy-runtime/env-config.js
+envsubst < /usr/share/caddy/env-config.js.template > /usr/share/caddy/env-config.js
 
 # Start Caddy
 exec caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
